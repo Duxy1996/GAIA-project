e1849216b9bcf236f46b39b87ad41692a19e9751
duxy1996 <durancdr96@gmail.com>
2018-09-22 13:41:53 +0200

    Added basic test GIN-gonic app

---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/contrib/static"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	// Set the router as the default one shipped with Gin
+	router := gin.Default()
+
+	// Serve frontend static files
+	router.Use(static.Serve("/", static.LocalFile("./views", true)))
+
+	// Setup route group for the API
+	api := router.Group("/api")
+	{
+		api.GET("/", func(c *gin.Context) {
+			c.JSON(http.StatusOK, gin.H{
+				"message": "pong",
+			})
+		})
+	}
+
+	api.GET("/jokes", JokeHandler)
+	api.POST("/jokes/like/:jokeID", LikeJoke)
+
+	// Start and run the server
+	router.Run(":3000")
+}
+
+func JokeHandler(c *gin.Context) {
+	c.Header("Content-Type", "application/json")
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Jokes handler not implemented yet",
+	})
+}
+
+// LikeJoke increments the likes of a particular joke Item
+func LikeJoke(c *gin.Context) {
+	c.Header("Content-Type", "application/json")
+	c.JSON(http.StatusOK, gin.H{
+		"message": "LikeJoke handler not implemented yet",
+	})
+}
